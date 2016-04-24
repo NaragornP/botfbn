282b75cd22030d397792d9782afae96b4de42707
naragorn <oikdiiii@gmail.com>
2016-04-24 15:28:04 +0700

    test search

--- app.js
+++ app.js
@@ -25,8 +25,10 @@ app.post('/webhook/', function (req, res) {
     if (event.message && event.message.text) {
       var text = event.message.text;
       console.log(text);
-      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+      // sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
       // Handle a text message from this sender
+      var search = "https://www.youtube.com/results?search_query="+text;
+      sendTextMessage(sender, search);
     }
   }
   res.sendStatus(200);
